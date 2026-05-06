ec5131b579cdb3e5d42a6595e0863e2506ed34d7
fanxiaoyu <fanxiaoyu1234@gmail.com>
2026-05-06 19:39:27 +0800

    fix: use __dirname in metro.config.js so dotenv finds .env during gradle builds

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 const { getDefaultConfig } = require('expo/metro-config');
 
 module.exports = getDefaultConfig(__dirname);
